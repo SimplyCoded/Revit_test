4eaf0d9dc4d7ba4cf112e553c66236988f0dae2a
Leander Spillmann <leander@hispeed.ch>
2023-06-06 15:00:21 +0200

    .py init

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""\
+This script foos quite well
+
+Usage:
+"""
+
+__author__ = "Simply"
+__version__ = "1.0.1"
+__maintainer__ = "Simply"
+__email__ = "leansink@aol.com"
+
+
+import os
+import sys
+
+
+def main():
+    return None
+
+
+if __name__ == '__main__':
+    main()
